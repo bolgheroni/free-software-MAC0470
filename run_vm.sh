d0ab98ce8f59c968506bcba7d9fba8de8a667d94
Roberto Bolgheroni <roberto.bolgheroni@ada.tech>
2024-04-07 14:38:04 -0300

    L-KERNEL-2 is created

--- run_vm.sh
+++ run_vm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-qemu-system-x86_64 -enable-kvm -nic user,hostfwd=tcp::2222-:22,smb=./shared_folder \
-                   -daemonize -m 4G -smp cores=4,cpus=4 ./image/L-KERNEL-BASIC
+qemu-system-x86_64 -enable-kvm -nic user,hostfwd=tcp::2222-:22 \
+                   -daemonize -m 4G -smp cores=4,cpus=4 ./image/L-KERNEL-2
